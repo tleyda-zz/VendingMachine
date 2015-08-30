345b5830b3a98b7d000458780f3b0c6e7426aa00
tleyda <troy.leyda@gmail.com>
2015-08-29 23:42:49 -0500

    Vending Machine Kata
    Vending machine recognizes coins.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -2,7 +2,33 @@
  * Created by tleyda on 8/29/2015.
  */
 public class VendingMachine {
+    public enum CoinType {
+        PENNY,
+        NICKEL,
+        DIME,
+        QUARTER,
+        UNKNOWN_COIN
+    }
+
     public String getDisplay() {
         return "INSERT COIN";
+    }
+
+    public CoinType detectCoin(Integer coinWeight) {
+        if ( coinWeight > 0 ) {
+            if ( coinWeight < 5 )
+                return CoinType.DIME;
+
+            if ( coinWeight < 10 )
+                return CoinType.PENNY;
+
+            if ( coinWeight < 15 )
+                return CoinType.NICKEL;
+
+            if ( coinWeight < 20 )
+                return CoinType.QUARTER;
+        }
+
+        return CoinType.UNKNOWN_COIN;
     }
 }
--- test/VendingMachineTest.java
+++ test/VendingMachineTest.java
@@ -8,4 +8,14 @@ public class VendingMachineTest {
         VendingMachine machine = new VendingMachine();
         assertEquals( "INSERT COIN", machine.getDisplay());
     }
+
+    @Test
+    public void vendingMachineIdentifiesCoinsByWeight() {
+        VendingMachine machine = new VendingMachine();
+        assertEquals( VendingMachine.CoinType.UNKNOWN_COIN, machine.detectCoin(0));
+        assertEquals( VendingMachine.CoinType.PENNY, machine.detectCoin(9));
+        assertEquals( VendingMachine.CoinType.NICKEL, machine.detectCoin(14));
+        assertEquals( VendingMachine.CoinType.DIME, machine.detectCoin(4));
+        assertEquals( VendingMachine.CoinType.QUARTER, machine.detectCoin(19));
+    }
 }
